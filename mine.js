1757154046df5fa76f11fdfe1bf79f6c90350a56
Favour Egwele <favoregwele@gmail.com>
2025-01-30 10:06:47 -1200

    Adding stuff to JavaScript

--- mine.js
+++ mine.js
@@ -12,3 +12,50 @@ const shyDevil = document.getElementById("button2");
 const meanSweetHeart = document.getElementById("button3");
 const displayText = document.getElementById("text");
 
+
+
+
+const path = [ 
+    {
+        id: 1,
+        text: "Show this instead.",
+        Options: [
+            {
+                text: "Excited",
+                setState: () => { confident += 5 },
+                nextPath: 2
+            },
+            {
+                text: "Anxious",
+                setState: () => { shy += 5 },
+                nextPath: 2
+            },
+            {
+                text: "Whatever...",
+                setState: () => { mean += 5 },
+                nextPath: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: "Testing Path two",
+        Options: [
+            {
+                text: "Button 1",
+                setState: () => { confident -= 5 },
+                nextPath: -1
+            },
+            {
+                text: "Button 2",
+                setState: () => { shy -= 5 },
+                nextPath: -1
+            },
+            {
+                text: "Button 3",
+                setState: () => { mean -= 5 },
+                nextPath: -1
+            }
+        ]
+    },
+]
